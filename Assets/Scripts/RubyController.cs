02b2281254902bcd7c013f48cd8d0a7b0f7038e2
corsantic <aydofurkan25@gmail.com>
2021-01-18 21:22:38 +0300

    added damagezone with damageable prefab
    script for damage and invincible timer for continous damage

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
-
     public readonly int maxHealth = 5;
-    
+    public float timeInvincible = 2.0f;
+
     public int health { get { return currentHealth; }}
     int currentHealth;
 
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -20,7 +22,6 @@ public class RubyController : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -28,6 +29,13 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if(isInvincible){
+            invincibleTimer -= Time.deltaTime;
+            if(invincibleTimer < 0){
+                isInvincible = false;
+            }
+        }
     }
 
     void FixedUpdate()
@@ -39,8 +47,14 @@ public class RubyController : MonoBehaviour
     }
     
     public void ChangeHealth(int amount)
-    {
-        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+    {   
+        if(amount < 0){
+            if(isInvincible) return;
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
+       currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
