423cc70d072d3d80a46257ca6c89d0a760b65a75
corsantic <aydofurkan25@gmail.com>
2021-01-19 20:09:54 +0300

    enemy controller animation change added

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,6 +11,8 @@ public class EnemyController : MonoBehaviour
 
     Rigidbody2D rigidbody2D;
 
+    Animator animator;
+
     float timer;
     int direction = 1;
     
@@ -19,6 +21,7 @@ public class EnemyController : MonoBehaviour
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
     
     private void Update() {
@@ -42,10 +45,14 @@ public class EnemyController : MonoBehaviour
         
         if (vertical)
         {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
             position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
             position.x = position.x + Time.deltaTime * speed * direction;
         }
         
